0cd6448c9f2f10572a8c305490281236a8fc1063
shootfirst <2782457525@qq.com>
2022-11-22 11:30:24 +0800

    Delete binary_tree directory

--- binary_tree/二叉树的最大深度.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
-        else return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
-    }
-
-    // 迭代法使用层序遍历即可
-}
