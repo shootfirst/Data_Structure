6f1180fefe159f44e11aee2a1e40589124bb0799
shootfirst <2782457525@qq.com>
2023-10-29 09:05:50 +0800

    Update 删除一次得到子数组最大和.cpp

--- algorithm/dp/01线性dp/code/删除一次得到子数组最大和.cpp
+++ algorithm/dp/01线性dp/code/删除一次得到子数组最大和.cpp
@@ -1,18 +1,18 @@
 class Solution {
-    // https://leetcode.cn/problems/maximum-subarray/
-    public int maxSubArray(int[] nums) {
-        int n = nums.length;
-
-        int [] dp = new int[n];
-        dp[0] = nums[0];
-        int res = nums[0];
+public:
+    int maximumSum(vector<int>& arr) {
+        int n = arr.size();
+        int dp[n][2];
+        dp[0][0] = arr[0];
+        dp[0][1] = 0;
+        int ans = arr[0];
 
         for (int i = 1; i < n; i++) {
-            if (dp[i - 1] + nums[i] <= nums[i]) dp[i] = nums[i];
-            else dp[i] = dp[i - 1] + nums[i];
-            res = Math.max(dp[i], res);
+            dp[i][0] = max(dp[i - 1][0] + arr[i], arr[i]);
+            dp[i][1] = max(dp[i - 1][0], dp[i - 1][1] + arr[i]);
+            ans = max(max(ans, dp[i][0]), dp[i][1]);
         }
 
-        return res;
+        return ans;
     }
-}
+};
